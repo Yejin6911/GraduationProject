254ab8ca1bc6348a69afe1e5caf1bc3121311450
yejin <cdjin6911@gmail.com>
2020-08-12 17:21:29 +0900

    login 수정

--- account/views.py
+++ account/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+
 
 def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("map:main")
+        else:
+            return render(request, "account/login.html", {'error': 'username or password is incorrect'})
     return render(request, "account/login.html")
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
