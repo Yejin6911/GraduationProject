461508c8df59e19071b690f1661d8e89718be410
yejin <cdjin6911@gmail.com>
2020-08-12 15:34:36 +0900

    Edit template
    템플릿 파일 위치 변경 및 수정

--- map/views.py
+++ map/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def main(request):
+    time = request.session.get_expiry_age()
+    print(time)
     return render(request, "map/main.html")
+
+def login(request):
+    return render(request, "map/login.html")
