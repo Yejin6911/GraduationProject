8586b7498ac11e3ada6f1db64b05399313914a9c
bominjang <qhdud9443@naver.com>
2020-08-14 14:24:08 +0900

    마커 + 이벤트 추가

--- map/views.py
+++ map/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
 
-# Create your views here.
+f = open("/Users/jangbomin/dev/GraduationProject/map/static/data.csv","r", encoding='cp949')
+lines = f.readlines()
+l = []
+for line in lines:
+    l.append(line.split(','))
+f.close()
+data = l[1:]
+
 def main(request):
     time = request.session.get_expiry_age()
     print(time)
-    return render(request, "map/main.html")
+    return render(request, "map/main.html", {'data':data})
