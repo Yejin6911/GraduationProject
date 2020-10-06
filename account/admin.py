f41794a1d46b5cb5f9c5a54d069038dd0a1c20a9
YEJIN <cdjin6911@gmail.com>
2020-10-06 18:47:59 +0900

    sms 전송 구현 완료

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from account.models import CustomUser, Station
+from account.models import CustomUser, Station, Guard
 from import_export.admin import ImportExportMixin
 
 admin.site.register(CustomUser)
@@ -9,3 +9,8 @@ class StationAdmin(ImportExportMixin, admin.ModelAdmin):
     pass
 
 admin.site.register(Station, StationAdmin)
+
+class GuardAdmin(ImportExportMixin, admin.ModelAdmin):
+    pass
+
+admin.site.register(Guard, GuardAdmin)
